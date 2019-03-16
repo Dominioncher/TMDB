2045a0ecadb4fff21a45ee01438ac2745aee84b9
BIGV10 <BIGV10@mail.ru>
2019-03-16 09:38:22 +0500

    updated main, describe

--- TMDB/main.py
+++ TMDB/main.py
@@ -1,8 +1,9 @@
-import pandas as pd
-from TMDB.describe import print_description
+import pandas as pd 
+from describe import print_description, file_description
 
 submission_data = pd.read_csv("data/sample_submission.csv", index_col=0)
 train_data = pd.read_csv("data/train.csv", index_col=0)
 test_data = pd.read_csv("data/test.csv", index_col=0)
 
 print_description(train_data)
+file_description(train_data, 'output.txt')
