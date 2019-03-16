8ee2f8b9e8040067827fec6ecc8b0e6b36323d5b
tanoker <tanok09@gmail.com>
2019-03-16 08:59:07 +0500

    clear main

--- TMDB/main.py
+++ TMDB/main.py
@@ -1,26 +1,7 @@
 import pandas as pd 
-import numpy as np 
-import datetime
 
-now = datetime.datetime.now()
-
-data1 = pd.read_csv("data/sample_submission.csv", index_col=0)
-data2 = pd.read_csv("data/train.csv", index_col=0)
-data3 = pd.read_csv("data/test.csv", index_col=0)
-
-#print(data2.shape)
-print(data2.info())
-data2['new_date'] = pd.to_datetime(data2['release_date'], format='%m/%d/%y')
-data2 = data2[data2.new_date > now]
-#data2 = data2.drop(columns=['belongs_to_collection', 'genres', 'homepage', 'imdb_id', 'overview', 'popularity', 'poster_path', 'production_companies','production_countries', 'spoken_languages', 'status', 'Keywords', 'cast', 'crew'])
-prep = data2['new_date']
-
-q = prep.describe()
-q1 = prep.unique().tolist()
-q2 = prep.nonzero()[0].shape
+submission_data = pd.read_csv("data/sample_submission.csv", index_col=0)
+train_data = pd.read_csv("data/train.csv", index_col=0)
+test_data = pd.read_csv("data/test.csv", index_col=0)
 
 
-names = list()
-for name, values in data2.iteritems():
-    print(values.describe())
-    names.append(name)
