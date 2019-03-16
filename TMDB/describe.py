83710d647380d8bcb68c68759373aa08439ad38f
BIGV10 <BIGV10@mail.ru>
2019-03-16 09:04:40 +0500

    changed description

--- TMDB/describe.py
+++ TMDB/describe.py
@@ -1,12 +1,5 @@
 import pandas as pd 
-import numpy as np
 
-#data1 = pd.read_csv("data/sample_submission.csv", index_col=0)
-data2 = pd.read_csv("data/train.csv", index_col=0)
-#data3 = pd.read_csv("data/test.csv", index_col=0)
-
-print(data2.info())
-names = list()
-for name, values in data2.iteritems():
-    print(values.describe(), '\n')
-    names.append(name)
+def print_description(data):
+    for name, values in data.iteritems():
+        print(values.describe(), '\n')
--- TMDB/main.py
+++ TMDB/main.py
@@ -1,7 +1,8 @@
 import pandas as pd 
+from describe import print_description
 
 submission_data = pd.read_csv("data/sample_submission.csv", index_col=0)
 train_data = pd.read_csv("data/train.csv", index_col=0)
 test_data = pd.read_csv("data/test.csv", index_col=0)
 
-
+print_description(train_data)
