5913de28635ebbfc341b8bf851382900b394afb1
tanoker <tanok09@gmail.com>
2019-03-30 09:01:31 +0500

    вес компаний

--- TMDB/Modules/DataPreparation/DataPreparation.py
+++ TMDB/Modules/DataPreparation/DataPreparation.py
@@ -7,6 +7,7 @@ from TMDB.Modules.Helpers.LabelEncoding import label_encode
 def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
     # data = data.drop(['name', 'ID', 'deadline', 'launched', 'currency', 'goal', 'pledged', 'usd pledged'], axis=1)
     # data = data.drop(['ID', 'goal', 'pledged', 'usd pledged'], axis=1)
+    data = label_encode(data)
     return data
 
 def dummy_code_genres(data: pd.DataFrame):
@@ -25,10 +26,25 @@ def dummy_code_production_companies(data: pd.DataFrame)-> pd.DataFrame:
 
 def weight_code_production_companies(data: pd.DataFrame)-> pd.DataFrame:
     data['production_companies'] = data['production_companies'].map(lambda x: sorted([d['name'] for d in get_dictionary(x)])).map(lambda x: ','.join(map(str, x)))
-    companies = data.production_companies.str.get_dummies(sep=',')
-    data = pd.concat([data, companies], axis=1, sort=False)
-    data = data.drop(columns=["production_companies"])
+    companies = data.production_companies.str.get_dummies(sep=',').columns.values.tolist()
 
+    for i, row in data.iterrows():
+        comp_names = row['production_companies'].split(sep=',')
+        revenue = 0
+        counter = 0
+        for company in comp_names:
+            filled_revenue = data[data['production_companies'].str.contains(company)]
+            if filled_revenue['revenue'].mean() == filled_revenue['revenue'].mean():
+                revenue = revenue + filled_revenue['revenue'].mean()
+                counter = counter + 1
+        if revenue == 0:
+            data.drop([i], axis=0)
+        else:
+            data.set_value(i, 'company_weight', float(revenue) / float(counter))
+
+    data['company_weight'] = (data['company_weight']-data['company_weight'].min())/(data['company_weight'].max()-data['company_weight'].min())
+    data = data.drop(columns=['production_companies'])
+    return data
 
 def label_coding(data1: pd.DataFrame, data2: pd.DataFrame, ) -> pd.DataFrame:
 #   data['genres'] = data['genres'].map(lambda x: sorted([d['name'] for d in preparation.get_dictionary(x)])).map(lambda x: ','.join(map(str, x)))
@@ -74,11 +90,6 @@ def data_split(data: pd.DataFrame, target_column_name: str):
     target = data[target_column_name]
     data = data.drop([target_column_name], axis=1)
     return train_test_split(data, target, test_size=0.33, random_state=42)
-
-def proper_data_split(data: pd.DataFrame, target_column_name: str):
-    target = data[target_column_name]
-    data = data.drop([target_column_name], axis=1)
-    return data, target
 
 # Используется в разбиении объекта
 def get_dictionary(s):
