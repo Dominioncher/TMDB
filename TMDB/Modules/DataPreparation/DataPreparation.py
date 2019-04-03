44ab4a3adc58ad6e8e50bf85d515829e0cf41a1d
Artyom <kushnirartyom@yandex.ru>
2019-04-03 20:50:52 +0500

    Notation

--- TMDB/Modules/DataPreparation/DataPreparation.py
+++ TMDB/Modules/DataPreparation/DataPreparation.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
+# В ЭТОМ ФАЙЛЕ МЕТОДЫ НЕ ДОБАВЛЯЕМ ВЫНОСИТЕ ИХ В ОТДЕЛЬНЫЕ ФАЙЛЫ
+
 # Подготовка данных + удаление ненужных колонок + добавление новых колонок
 def feature_engineering(data: pd.DataFrame) -> pd.DataFrame:
     return data
